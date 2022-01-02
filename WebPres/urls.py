0f0a8719fb90b2c14b14eb33d71e520d6fc0d605
Rony Man <ronyman.com@outlook.com>
2022-01-02 15:48:41 +1100

    201148

--- WebPres/urls.py
+++ WebPres/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     # Core_urls
 
     path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
