4204624733ed074902cdc9a1d8f4ce99d0b21c4b
Rony Man <ronyman.com@outlook.com>
2022-01-09 16:33:51 +1100

    09012022433

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
 from . import views
 from django.urls import path
 from apps.user import views as user_views
+from django.views.generic import ListView, DetailView
+from django.urls import path
+from .views import HomeView, PostView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='index'),
-    path('<slug:slug>/', views.PostDetail.as_view(),  name='post_detail'),
+    path('posts/index/', HomeView.as_view(), name='index'),
+    #path('<slug:slug>/', views.PostDetail.as_view(),  name='post_detail'),
     #path('add_post/', views.AddPostView.as_view(),  name='add_post'),
+    path('post/create/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
