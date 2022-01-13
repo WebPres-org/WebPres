e3a53e85e89218a24d68984c1b2e393a05c6cb7f
Rony Man <ronyman.com@outlook.com>
2022-01-14 00:38:03 +1100

    140120221237

--- apps/hello_world/admin.py
+++ apps/hello_world/admin.py
@@ -7,7 +7,7 @@ class HelloWorldAdmin(AdminSite):
     # Text to put in each page's <h1> (and above login form).
     admin.site.site_header = 'WebPres.org'
     admin.site.site_title = 'Cms Site Builder | WebPres,org'
-    admin.site.index_title = 'Hello World'
+    admin.site.index_title = 'WebPres'
 
 
 
